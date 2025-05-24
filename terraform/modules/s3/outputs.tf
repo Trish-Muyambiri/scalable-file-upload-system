fc1c3afc8d2adcad6763be62280c9ce32d702e21
Patricia Muyambiri <pmuyambiri@gmail.com>
2025-05-24 08:08:00 +0200

    add outputs for s3 bucket name and arn

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,1 +1,20 @@
-# outputs.tf for s3 module
+output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.quarantine_bucket.id
+}
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.quarantine_bucket.arn
+}
+
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.quarantine_bucket.bucket
+}
+
+output "kms_key_arn" {
+  description = "The ARN of the KMS key used for encryption"
+  value       = aws_kms_key.quarantine_bucket.arn
+}
+
