54e583b42be4e310229c7ec425c0c401788f6765
Patricia Muyambiri <pmuyambiri@gmail.com>
2025-05-24 11:58:26 +0200

    add variables.tf to root module

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Project = "FileUpload"
+    Owner   = "Patricia"
+  }
+}
