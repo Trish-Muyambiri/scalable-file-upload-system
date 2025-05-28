fb470a49eb0d28467fe8de6186c6f32eda698797
Patricia Muyambiri <pmuyambiri@gmail.com>
2025-05-28 18:09:44 +0200

    add outputs to be used by lambda function

--- terraform/modules/s3/quarantine_bucket/outputs.tf
+++ terraform/modules/s3/quarantine_bucket/outputs.tf
@@ -1,3 +1,4 @@
+/** 
 output "bucket_id" {
   description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.quarantine_bucket.id
@@ -13,8 +14,35 @@ output "bucket_name" {
   value       = "${var.bucket_name}-${random_pet.suffix.id}"
 }
 
-output "kms_key_arn" {
-  description = "The ARN of the KMS key used for encryption"
-  value       = aws_kms_key.quarantine_bucket.arn
+
+
+**/
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.quarantine_bucket.id
 }
 
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.quarantine_bucket.arn
+}
+
+output "bucket_domain_name" {
+  description = "The bucket domain name"
+  value       = aws_s3_bucket.quarantine_bucket.bucket_domain_name
+}
+
+output "bucket_regional_domain_name" {
+  description = "The bucket regional domain name"
+  value       = aws_s3_bucket.quarantine_bucket.bucket_regional_domain_name
+}
+
+output "bucket_region" {
+  description = "The AWS region of the bucket"
+  value       = aws_s3_bucket.quarantine_bucket.region
+}
+
+output "upload_prefix" {
+  description = "Prefix for uploads in the bucket"
+  value       = "uploads/"
+}
