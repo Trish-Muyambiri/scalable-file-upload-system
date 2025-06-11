b50f7faea6a2151124a67da4d277ec4c72b13c6f
Patricia Muyambiri <pmuyambiri@gmail.com>
2025-06-11 14:39:33 +0200

    add variables to support API Gateway and Lambda wiring

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,47 @@ variable "tags" {
     Owner   = "Patricia"
   }
 }
+
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "scalable-file-upload"
+}
+
+
+variable "api_name" {
+  description = "Name of the API Gateway"
+  type        = string
+  default     = "document-upload-api"
+}
+
+variable "stage_name" {
+  description = "API Gateway stage name"
+  type        = string
+  default     = "prod"
+}
+
+variable "authorization_type" {
+  description = "API Gateway authorization type"
+  type        = string
+  default     = "NONE"
+}
+
+variable "enable_api_logging" {
+  description = "Enable API Gateway logging"
+  type        = bool
+  default     = true
+}
+
+variable "enable_xray_tracing" {
+  description = "Enable X-Ray tracing"
+  type        = bool
+  default     = false
+}
