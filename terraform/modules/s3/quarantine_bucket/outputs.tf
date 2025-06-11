425bb4acba05ee8928868ff5945dc032c73ac306
Patricia Muyambiri <pmuyambiri@gmail.com>
2025-06-11 14:38:17 +0200

    expose bucket name and ARN as outputs

--- terraform/modules/s3/quarantine_bucket/outputs.tf
+++ terraform/modules/s3/quarantine_bucket/outputs.tf
@@ -1,22 +1,4 @@
-/** 
-output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.quarantine_bucket.id
-}
 
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.quarantine_bucket.arn
-}
-
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = "${var.bucket_name}-${random_pet.suffix.id}"
-}
-
-
-
-**/
 output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.quarantine_bucket.id
